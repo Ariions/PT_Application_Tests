163b09f06c97ccb45f00720fdb267effe9679bc5
Ariions <arnas.bendinskas@gmail.com>
2020-12-03 14:17:11 +0200

    Starting test just to create terain and check its properties

--- Assets/Tests/PlayModeTests/TerrainTests.cs
+++ Assets/Tests/PlayModeTests/TerrainTests.cs
@@ -10,8 +10,11 @@ using UnityEditor;
 public class TerrainTests : TestTemplate
 {
     //List of prefabs
-    GameObject Prefab_Terrain => GenericTestMethods.GetPrefab("Terrain");
+    GameObject Prefab_TerrainBase => GenericTestMethods.GetPrefab("BaseTerrain");
+    GameObject Prefab_TerrainTree => GenericTestMethods.GetPrefab("TerrainTree");
     GameObject Prefab_MainCamera => GenericTestMethods.GetPrefab("Gameplay_Camera");
+
+    GameObject listener;
 
     [SetUp]
     public override void Setup()
@@ -19,13 +22,57 @@ public class TerrainTests : TestTemplate
         base.Setup();
         GameManager.InitializeTestingEnvironment(false, false, true, false, false);
         Object.Instantiate(Prefab_MainCamera);
+        InitializeAudioListener();
     }
 
-    tets
 
     [Test]
     public void _01TerrainPrefabExists()
     {
-        Assert.NotNull(Prefab_Terrain);
+        Assert.NotNull(Prefab_TerrainBase);
+    }
+
+    [Test]
+    public void _02CreateTerrainGameObjectAPICheck()
+    {
+        GameObject spaceship = (GameObject)Terrain.CreateTerrainGameObject(new TerrainData());
+        spaceship.name = "NewTarrain";
+        Assert.NotNull(GameObject.Find("NewTarrain"));
+    }
+
+    [Test]
+    public void _03TerrainPrefabComponentTransform()
+    {
+        Assert.IsNotNull(Prefab_TerrainBase.GetComponent<Transform>());
+    }
+
+    [Test]
+    public void _04TerrainPrefabComponentTerrain()
+    {
+        Assert.IsNotNull(Prefab_TerrainBase.GetComponent<Terrain>());
+    }
+
+    [Test]
+    public void _04TerrainPrefabComponentTerrainCollider()
+    {
+        Assert.IsNotNull(Prefab_TerrainBase.GetComponent<TerrainCollider>());
+    }
+
+    [TearDown]
+    public virtual void Teardown()
+    {
+        GenericTestMethods.ClearScene();
+
+        if (listener)
+            Object.DestroyImmediate(listener);
+    }
+
+    private void InitializeAudioListener()
+    {
+        //Disables Audio Listener warning spam
+        listener = new GameObject("TestListener");
+        listener.AddComponent<AudioListener>();
+        listener.AddComponent<TestAudioListener>();
+        Object.DontDestroyOnLoad(listener);
     }
 }
